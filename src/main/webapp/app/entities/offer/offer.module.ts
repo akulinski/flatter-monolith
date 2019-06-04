f275579694e74b3e3b74544ce8322d9080f7d2bb
Albert <albert.kulinski@hotmail.com>
2019-06-04 10:01:48 +0200

    Changed offer details view. Added icons for offer details. Used flex.
    resolves #19

--- src/main/webapp/app/entities/offer/offer.module.ts
+++ src/main/webapp/app/entities/offer/offer.module.ts
@@ -11,12 +11,19 @@ import {
   offerRoute,
   OfferUpdateComponent
 } from './';
-import {MatGridListModule, MatTabsModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatIconModule, MatListModule,
+  MatTabsModule
+} from '@angular/material';
 
 const ENTITY_STATES = [...offerRoute, ...offerPopupRoute];
 
 @NgModule({
-  imports: [FlatterservermonolithSharedModule, RouterModule.forChild(ENTITY_STATES), MatGridListModule, MatTabsModule],
+  imports: [FlatterservermonolithSharedModule, RouterModule.forChild(ENTITY_STATES), MatGridListModule, MatTabsModule, MatCardModule, MatDividerModule, MatButtonModule, MatIconModule, MatListModule],
   declarations: [OfferComponent, OfferDetailComponent, OfferUpdateComponent, OfferDeleteDialogComponent, OfferDeletePopupComponent],
   entryComponents: [OfferComponent, OfferUpdateComponent, OfferDeleteDialogComponent, OfferDeletePopupComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
