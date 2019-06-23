154ce84f8c3685c63ee8387a338b48b795712b53
Tomasz Gaweł <tomekgawel95@gmail.com>
2019-06-23 14:09:28 +0200

    Fs 14 (#41)
    * Initial version of home view
    
    * Home view redesign

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -3,9 +3,11 @@ import { RouterModule } from '@angular/router';
 
 import { FlatterservermonolithSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import {MatButtonModule, MatGridListModule, MatCardModule, MatCheckboxModule} from '@angular/material';
 
 @NgModule({
-  imports: [FlatterservermonolithSharedModule, RouterModule.forChild([HOME_ROUTE])],
+  imports: [FlatterservermonolithSharedModule, RouterModule.forChild([HOME_ROUTE]), MatButtonModule, MatCheckboxModule, MatCardModule, MatGridListModule],
+
   declarations: [HomeComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
